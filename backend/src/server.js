f9f0dd46d96dadd3ae343ada5677168723759190
bhuiyan.minhajul@gmail.com <bhuiyan.minhajul@gmail.com>
2025-07-16 13:23:00 +0600

    #9: Completing user.controller.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,7 +5,7 @@ import cors from "cors";
 import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
-//import userRoutes from "./routes/user.route.js";
+import userRoutes from "./routes/user.route.js";
 //import chatRoutes from "./routes/chat.route.js";
 
 import { connectDB } from "./lib/db.js";
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-//app.use("/api/users", userRoutes);
+app.use("/api/users", userRoutes);
 //app.use("/api/chat", chatRoutes);
 
 if (process.env.NODE_ENV === "production") {
