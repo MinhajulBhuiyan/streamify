968dff10c9befa82631cf31903add3ffb0d8be5c
bhuiyan.minhajul@gmail.com <bhuiyan.minhajul@gmail.com>
2025-07-17 13:55:06 +0600

    #11: Updating api.js

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -5,6 +5,15 @@ export const signup = async (signupData) => {
   return response.data;
 };
 
+export const login = async (loginData) => {
+  const response = await axiosInstance.post("/auth/login", loginData);
+  return response.data;
+};
+export const logout = async () => {
+  const response = await axiosInstance.post("/auth/logout");
+  return response.data;
+};
+
 export const getAuthUser = async () => {
   try {
     const res = await axiosInstance.get("/auth/me");
@@ -14,3 +23,9 @@ export const getAuthUser = async () => {
     return null;
   }
 };
+
+export const completeOnboarding = async (userData) => {
+  const response = await axiosInstance.post("/auth/onboarding", userData);
+  return response.data;
+};
+
