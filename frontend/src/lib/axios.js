6c1c543ed4c5f95702f0dd3d5f085c4acb1ab276
bhuiyan.minhajul@gmail.com <bhuiyan.minhajul@gmail.com>
2025-07-24 23:25:00 +0600

    #16: Correcting Deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const BASE_URL = import.meta.env.MODE === "development"
-  ? "http://localhost:5001"
-  : import.meta.env.VITE_API_URL;
+  ? "http://localhost:5001/api"
+  : import.meta.env.VITE_API_URL + "/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
